d1466f0f0bf227fc97d11d5bee8f9c95a0f9669d
nataliafb <nataliafigueroabernal@gmail.com>
2018-07-23 19:45:45 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,5 @@
-# Build your say_hello method here
+I call on the method, say_hello, and give it the string "Hello Ruby Programmer" 
+say_hello("Hello Ruby Programmer")
+ 
+The method prints this text to the screen:
+Hello Ruby Programmer!
