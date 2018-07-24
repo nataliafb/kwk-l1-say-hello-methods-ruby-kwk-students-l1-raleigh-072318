674478d48dcec4a78d7b37939867a832141ae0fc
nataliafb <nataliafigueroabernal@gmail.com>
2018-07-24 14:07:18 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,3 @@
-I call on the method, say_hello, and give it the string "Hello Ruby Programmer" 
-say_hello("Hello Ruby Programmer")
- 
-The method prints this text to the screen:
-Hello Ruby Programmer!
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
+end 
